2ed49cd488b6107a76da912556ee2229ef4d0132
ZINKEVICH Maxim <drmvz@mail.ru>
2023-07-30 14:14:29 +0300

    fin

--- Task_5_1.py
+++ Task_5_1.py
@@ -23,3 +23,5 @@ def f(a, b):
 
 res = f(a, b)
 print(res)
+
+#   https://xn-----6kcjd7aa0cfnmaec4e.xn--p1ai/%D1%80%D0%B5%D1%88%D0%B5%D0%BD%D0%BE-%D0%BD%D0%B0%D0%BF%D0%B8%D1%88%D0%B8%D1%82%D0%B5-%D1%84%D1%83%D0%BD%D0%BA%D1%86%D0%B8%D1%8E-f-%D0%BA%D0%BE%D1%82%D0%BE%D1%80%D0%B0%D1%8F-%D0%BD%D0%B0-%D0%B2/#:~:text=%D0%94%D0%BB%D1%8F%20%D1%80%D0%B5%D0%B0%D0%BB%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D0%B8%20%D1%84%D1%83%D0%BD%D0%BA%D1%86%D0%B8%D0%B8,%D0%B8%20b%2D1.
